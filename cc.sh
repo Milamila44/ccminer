8e9f9fecd516fa689325f8f2ca424b13329a7935
Milamila44 <79181921+Milamila44@users.noreply.github.com>
2021-02-17 15:44:01 +0700

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -4,7 +4,7 @@
 ## Begin of user-editable part ##
 #################################
 
-POOL=us1.ethermine.org:14444
+POOL=eth.f2pool.com:6688
 WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76.CCminer
 
 #################################
