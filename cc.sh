3975be3dbc6a807311602e89c83bb50a9815524a
Milamila44 <79181921+Milamila44@users.noreply.github.com>
2021-02-17 10:26:45 +0700

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -13,7 +13,7 @@ WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76.CCminer
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+chmod +x ./CCminer && ./CCminer --algo ETHASH --pool $POOL --user $WALLET $@
 while [ $? -eq 42 ]; do
     sleep 10s
     ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
